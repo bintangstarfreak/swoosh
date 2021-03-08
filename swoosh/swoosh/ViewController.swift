f8cca2c455d53d7debeca81ab162047f7ab155ac
bintangaria <bintangstarfreak27@gmail.com>
2021-03-08 20:09:03 +0700

    Initial Commit

---
+++ swoosh/swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Bintang Aria Ramadhan on 08/03/21.
+//  Copyright © 2021 Coursenet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
